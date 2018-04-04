9bc76bb4290211d0d07bd3e06c2af1851aeaeb2a
Taegeon <31300248+Taegeon@users.noreply.github.com>
2018-04-03 23:12:27 -0400

    Update MultipartUtility.java

--- ArduinoStore/src/arduinostore/MultipartUtility.java
+++ ArduinoStore/src/arduinostore/MultipartUtility.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package arduinostore;
 
-/**
- * @author orion
- */
+package arduinostore;
  
 import java.io.BufferedReader;
  
