ccddf4cf32f7be21934a14426e5b00497a4069c9
Taegeon <31300248+Taegeon@users.noreply.github.com>
2018-04-03 23:11:22 -0400

    Update ArduinoStoreGUI.java

--- ArduinoStore/src/arduinostore/ArduinoStoreGUI.java
+++ ArduinoStore/src/arduinostore/ArduinoStoreGUI.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arduinostore;
 
 import javax.swing.*;
 import java.io.*;
 import java.io.IOException;
 import java.util.List;
-/**
- *
- * @author orion
- */
+
 public class ArduinoStoreGUI extends javax.swing.JFrame {
     
     private int index = -1;
